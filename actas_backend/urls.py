6639bc799c89ebcb37f76a4842b85b136fea9c40
Toño <ajsuarezf@unal.edu.co>
2019-05-06 07:22:53 -0500

    Added council_minutes base and MongoDB basic configuration

--- actas_backend/urls.py
+++ actas_backend/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('council_minutes/', include('council_minutes.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ council_minutes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CouncilMinutesConfig(AppConfig):
+    name = 'council_minutes'
