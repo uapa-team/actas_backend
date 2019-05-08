da6cac361ed7d0ff161873baf1cd6fd16ed76500
adcorredorm <adcorredorm@unal.edu.co>
2019-05-08 16:28:58 -0500

    Dummy POST request

--- council_minutes/urls.py
+++ council_minutes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('request/', views.request, name='request'),
+    path('request/<int:id>', views.post, name='post')
 ]
--- council_minutes/views.py
+++ council_minutes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Test
 from .helpers import QuerySetEncoder
+import json
+from django.views.decorators.csrf import csrf_exempt #Esto va solo para evitar la verificacion de django
 
 
 def index(request):
@@ -11,3 +13,11 @@ def request(request):
     req = Test.objects
     
     return JsonResponse(req, safe=False, encoder=QuerySetEncoder)
+
+@csrf_exempt #Esto va solo para evitar la verificacion de django
+def post(request, id):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        return HttpResponse(json.dumps(data), status=200)
+    else:
+        return JsonResponse({"not post":"not post"})
