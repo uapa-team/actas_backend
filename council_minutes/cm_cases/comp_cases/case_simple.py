46a987d9ae375ea1dbec4875a40bfc49b22c391e
Laura Molina <ldmolinav@unal.edu.co>
2019-05-29 10:04:54 -0500

    case AP

--- council_minutes/cm_cases/comp_cases/case_simple.py
+++ council_minutes/cm_cases/comp_cases/case_simple.py
@@ -11,3 +11,61 @@ class simple():
     @staticmethod
     def case_REINGRESO_PREGRADO(request, docx):
         raise NotImplementedError
+
+    @staticmethod
+    def case_CAMBIO_DE_PERFIL_POSGRADO(request, docx):
+        large_program = ''
+        for p in Request.PROGRAM_CHOICES:
+            if p[0] == request.academic_program:
+                large_program = p[1]
+                break
+        para = docx.add_paragraph()
+        para.add_run('El Consejo de Facultad ')
+        if request.approval_status == 'AP':
+            para.add_run('APRUEBA').font.bold = True
+        else:
+            para.add_run('NO APRUEBA').font.bold = True
+        para.add_run(' el traslado del plan de estudios de ')
+        para.add_run(request.detail_cm['from_node'])
+        para.add_run(' al plan de estudios de ')
+        para.add_run(request.detail_cm['to_node'])
+        para.add_run(' de ' + large_program + ' debido a que ')
+        para.add_run(request.justification + '.')
+         
+    @staticmethod
+    def case_AMPLIACION_DE_LA_FECHA_DE_PAGO_DE_DERECHOS_ACADEMICOS_POSGRADO(request, docx):
+        para = docx.add_paragraph()
+        para.add_run('El Consejo de Facultad ')
+        if request.approval_status == 'AP':
+            para.add_run('APRUEBA').font.bold = True
+        else:
+            para.add_run('NO APRUEBA').font.bold = True
+        para.add_run(' presentar con concepto positivo al Comité de Matriculas de la Sede')
+        para.add_run(' Bogotá, la expedición de un único recibo correspondiente a los')
+        para.add_run(' derechos académicos y administrativos para el periodo académico ')
+        para.add_run(request.academic_period + ' debido a que ')
+        para.add_run(request.justification + '.')
+
+    @staticmethod
+    def case_REEMBOLSO_PREGRADO(request, docx):
+        para = docx.add_paragraph()
+        para.add_run('El Consejo de Facultad ')
+        if request.approval_status == 'AP':
+            para.add_run('APRUEBA').font.bold = True
+        else:
+            para.add_run('NO APRUEBA ').font.bold = True
+            para.add_run('reembolsar {} créditos al estudiante {}.'.format(request.detail_cm['credits'], request.justification))
+
+    @staticmethod
+    def case_GENERACION_DE_RECIBO_UNICO_DE_PAGO_POSGRADO(request, docx):
+        para = docx.add_paragraph()
+        para.add_run('El Consejo de Facultad ')
+        if request.approval_status == 'AP':
+            para.add_run('APRUEBA ').font.bold = True
+            #No se si siempre es "porque se justifica debidamente la solicitud" o si existen más casos
+            para.add_run('presentar con concepto {} al Comité de Matrículas de la Sede Bogotá, la expedición de un único recibo correspondiente a saldos pendientes de matrícula para el periodo académico {}, porque justifica debidamente la solicitud. {}'.format(request.detail_cm['concept'],request.academic_period, request.observation)) 
+        else:
+            para.add_run('NO APRUEBA ').font.bold = True
+
+        
+    
