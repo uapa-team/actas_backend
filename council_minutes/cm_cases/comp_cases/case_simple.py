e1ec75b682d3f435e822ee9c79b8b2d9eee444c0
Fortich <ajsuarezf@unal.edu.co>
2019-05-29 08:22:27 -0500

    Generating CPERPRE NP. Finished CPERPRE.

--- council_minutes/cm_cases/comp_cases/case_simple.py
+++ council_minutes/cm_cases/comp_cases/case_simple.py
@@ -8,10 +8,16 @@ class simple():
     def case_CANCELACION_DE_PERIODO_ACADEMICO_PREGRADO(request, docx):
         para = docx.add_paragraph()
         para.add_run('El Consejo de Facultad ')
-        para.add_run('APRUEBA').font.bold = True
+        if request.approval_status == 'AP':
+            para.add_run('APRUEBA').font.bold = True
+        else:
+            para.add_run('NO APRUEBA ').font.bold = True
         para.add_run(' cancelar el periodo académico ' + request.academic_period)
-        para.add_run(', porque justifica documentalmente la fuerza mayor o caso fortuito. ')
-        para.add_run('(Artículo 18 del Acuerdo 008 del Consejo Superior Universitario).')
+        if request.approval_status == 'AP':
+            para.add_run(', debido a que justifica documentalmente la fuerza mayor o caso fortuito.')
+        else:
+            para.add_run(', debido a que ' + request.justification)
+        para.add_run(' (Artículo 18 del Acuerdo 008 del Consejo Superior Universitario).')
         para.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
 
     @staticmethod
