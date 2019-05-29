8ba397ca494ac26cccc13b4f7f485753e1b02beb
Fortich <ajsuarezf@unal.edu.co>
2019-05-29 08:33:07 -0500

    Generating CINFPRE NP. Finished CINFPRE.

--- council_minutes/cm_cases/comp_cases/case_simple.py
+++ council_minutes/cm_cases/comp_cases/case_simple.py
@@ -16,7 +16,15 @@ class simple():
     def case_CARGA_INFERIOR_A_LA_MINIMA_PREGRADO(request, docx):
         para = docx.add_paragraph()
         para.add_run('El Consejo de Facultad ')
-        para.add_run('APRUEBA').font.bold = True
+        if request.approval_status == 'AP':
+            para.add_run('APRUEBA').font.bold = True
+        else:
+            para.add_run('APRUEBA').font.bold = True
         para.add_run(' cursar el periodo académico ' + request.academic_period)
-        para.add_run(' con un número de créditos inferior al mínimo exigido, porque justifica debidamente la ')
-        para.add_run('solicitud (Artículo 10 del Acuerdo 008 de 2008 del Consejo Superior Universitario).')
+        para.add_run(' con un número de créditos inferior al mínimo exigido, debido a que ')
+        if request.approval_status == 'AP':
+            para.add_run(' justifica debidamente la solitud.')
+        else:
+             para.add_run(request.justification)
+        para.add_run(' (Artículo 10 del Acuerdo 008 de 2008 del Consejo Superior Universitario).')
+        para.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY 
