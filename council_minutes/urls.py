eb3c49ee1045aa4d7e59e50dffe2bdab26054b2c
Daniel Escobar <daescobarp@unal.edu.co>
2019-06-06 15:59:32 -0500

    Added some PEP8

--- council_minutes/urls.py
+++ council_minutes/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('read', views.filter_request, name='filter_request'),
     path('insert', views.insert_request, name='insert_request'),
     path('generate/<cm_id>', views.docx_gen_by_id, name='docx_gen'),
+    path('update/<cm_id>', views.update_cm, name='update_request'),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
--- council_minutes/views.py
+++ council_minutes/views.py
@@ -8,32 +8,34 @@ from .helpers import QuerySetEncoder
 import os
 import json
 from mongoengine.errors import ValidationError
-from django.views.decorators.csrf import csrf_exempt #Esto va solo para evitar la verificacion de django
+# Esto va solo para evitar la verificacion de django
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
     return HttpResponse("¡Actas trabajando!")
 
-@csrf_exempt #Esto va solo para evitar la verificacion de django
+
+@csrf_exempt  # Esto va solo para evitar la verificacion de django
 def filter_request(request):
     if request.method == 'POST':
-        #Generic Query for Request model
-        #To make a request check http://docs.mongoengine.org/guide/querying.html#query-operators
+        # Generic Query for Request model
+        # To make a request check http://docs.mongoengine.org/guide/querying.html#query-operators
         params = json.loads(request.body)
         response = Request.objects.filter(**params).order_by('req_acad_prog')
         return JsonResponse(response, safe=False, encoder=QuerySetEncoder)
-    
+
     else:
         return HttpResponse('Bad Request', status=400)
 
 
-@csrf_exempt #Esto va solo para evitar la verificacion de django
+@csrf_exempt  # Esto va solo para evitar la verificacion de django
 def insert_request(request):
     if request.method == 'POST':
         new_request = Request().from_json(Translator.translate(request.body))
         try:
             response = new_request.save()
-            
+
             return HttpResponse(request.body, status=201)
 
         except ValidationError as e:
@@ -44,14 +46,19 @@ def insert_request(request):
     else:
         return HttpResponse('Bad Request', status=400)
 
+
 @csrf_exempt
 def docx_gen_by_id(request, cm_id):
     filename = 'public/acta' + cm_id + '.docx'
     try:
-        request_by_id = Request.objects.get(id = cm_id)
+        request_by_id = Request.objects.get(id=cm_id)
     except mongoengine.DoesNotExist:
         return HttpResponse('Does not exist', status=404)
     generator = CouncilMinuteGenerator()
     generator.add_case_from_request(request_by_id)
     generator.generate(filename)
     return HttpResponse(filename)
+
+
+@ccsrf_exempt
+def update_cm(request, cm_id):
