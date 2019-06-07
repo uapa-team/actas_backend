edd853d22d57276829b52065958347983e7f4c08
Daniel Escobar <daescobarp@unal.edu.co>
2019-06-07 10:29:51 -0500

    More PEP8 in docx and spliter

--- council_minutes/docx.py
+++ council_minutes/docx.py
@@ -2,6 +2,7 @@ from docx import Document
 from docx.shared import Inches
 from .cm_cases.spliter import CasesSpliter
 
+
 class CouncilMinuteGenerator():
 
     def __init__(self):
@@ -9,10 +10,9 @@ class CouncilMinuteGenerator():
         self.document = Document()
         self.document.styles['Normal'].font.name = 'Ancizar Sans'
         self.case_count = 0
-        
 
     def add_case_from_request(self, request):
         self.spliter.request_case(request, self.document)
-        
+
     def generate(self, filename):
         self.document.save(filename)
