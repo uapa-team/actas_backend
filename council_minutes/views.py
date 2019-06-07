d718bc52ac61db33129b35b818922f3a3ca70000
Daniel Escobar <daescobarp@unal.edu.co>
2019-06-07 10:13:50 -0500

    Added update/<id> endpoint working propertly

--- council_minutes/views.py
+++ council_minutes/views.py
@@ -1,18 +1,16 @@
+import json
+import datetime
 import mongoengine
-from django.shortcuts import render
+from mongoengine.errors import ValidationError
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Request
 from .helpers import QuerySetEncoder, Translator
 from .docx import CouncilMinuteGenerator
-from .helpers import QuerySetEncoder
-import os
-import json
-from mongoengine.errors import ValidationError
 # Esto va solo para evitar la verificacion de django
-from django.views.decorators.csrf import csrf_exempt
 
 
-def index(request):
+def index():
     return HttpResponse("¡Actas trabajando!")
 
 
@@ -34,15 +32,12 @@ def insert_request(request):
     if request.method == 'POST':
         new_request = Request().from_json(Translator.translate(request.body))
         try:
-            response = new_request.save()
-
+            new_request.save()
             return HttpResponse(request.body, status=201)
-
         except ValidationError as e:
             print()
             print(e)
             return HttpResponse(e.message, status=400)
-
     else:
         return HttpResponse('Bad Request', status=400)
 
@@ -60,5 +55,61 @@ def docx_gen_by_id(request, cm_id):
     return HttpResponse(filename)
 
 
-@ccsrf_exempt
+@csrf_exempt
 def update_cm(request, cm_id):
+    if request.method == 'PATCH':
+        try:
+            acta = Request.objects.get(id=cm_id)
+        except mongoengine.DoesNotExist:
+            return HttpResponse('Does not exist', status=404)
+        json_body = json.loads(request.body.decode('utf-8'))
+        if hasattr(acta, 'old'):
+            old = acta.old
+        else:
+            old = []
+        old_obj = {}
+        some_change = False
+        if acta.type != json_body['type']:
+            some_change = some_change or True
+            old_obj.update({'type': acta.type})
+            acta.type = json_body['type']
+        if acta.student_name != json_body['student_name']:
+            some_change = some_change or True
+            old_obj.update({'student_name': acta.student_name})
+            acta.student_name = json_body['student_name']
+        if acta.approval_status != json_body['approval_status']:
+            some_change = some_change or True
+            old_obj.update({'approval_status': acta.approval_status})
+            acta.approval_status = json_body['approval_status']
+        if acta.student_dni != json_body['student_dni']:
+            some_change = some_change or True
+            old_obj.update({'student_dni': acta.student_dni})
+            acta.student_dni = json_body['student_dni']
+        if acta.student_dni_type != json_body['student_dni_type']:
+            some_change = some_change or True
+            old_obj.update({'student_dni_type': acta.student_dni_type})
+            acta.student_dni_type = json_body['student_dni_type']
+        if acta.academic_period != json_body['academic_period']:
+            some_change = some_change or True
+            old_obj.update({'academic_period': acta.academic_period})
+            acta.academic_period = json_body['academic_period']
+        if acta.academic_program != json_body['academic_program']:
+            some_change = some_change or True
+            old_obj.update({'academic_program': acta.academic_program})
+            acta.academic_program = json_body['academic_program']
+        if acta.justification != json_body['justification']:
+            some_change = some_change or True
+            old_obj.update({'justification': acta.justification})
+            acta.justification = json_body['justification']
+        if acta.detail_cm != json_body['detail_cm']:
+            some_change = some_change or True
+            old_obj.update({'detail_cm': acta.detail_cm})
+            acta.detail_cm = json_body['detail_cm']
+        if some_change:
+            old_obj.update({'user_who_update': acta.user})
+            old_obj.update({'datetime_update': datetime.datetime.now()})
+            old.append(old_obj)
+            acta.old = old
+            acta.save()
+            return HttpResponse('Changes updated successfully', status=200)
+        return HttpResponse('No changes detected', status=204)
