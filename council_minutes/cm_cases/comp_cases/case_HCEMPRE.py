9ad5caaa8002528cd6200f14fc5a93ce9ce11e7c
Laura Daniela Molina Villar <ldmolinav@unal.edu.co>
2019-06-10 22:30:11 -0500

    add case equivalence and recognition

--- council_minutes/cm_cases/comp_cases/case_HCEMPRE.py
+++ council_minutes/cm_cases/comp_cases/case_HCEMPRE.py
@@ -16,26 +16,38 @@ class HCEMPRE():
         para.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
         para.add_run('El Consejo de Facultad ')
         if request.approval_status == 'AP':
-            para.add_run('APRUEBA: ').font.bold = True
+            para.add_run('APRUEBA:').font.bold = True
+            para.paragraph_format.space_after = Pt(0)
             HCEMPRE.case_HOMOLOGACION_CONVALIDACION_EQUIVALENCIA_PREGRADO_AP(request, docx, para)
         else:
             HCEMPRE.case_HOMOLOGACION_CONVALIDACION_EQUIVALENCIA_PREGRADO_NA(request, docx, para)
     
     @staticmethod
     def case_HOMOLOGACION_CONVALIDACION_EQUIVALENCIA_PREGRADO_AP(request, docx, paragraph):      
-        if request.detail_cm['homologation']:
+        if 'homologation' in request.detail_cm:
             Item1 = 'Homologar en el periodo académico {}, la(s) siguiente(s) asignatura(s) cursada(s)'
             Item2 = ' en el programa {} de la {}, de la siguiente manera (Artículo 35 del Acuerdo 008 de 2008 del Consejo Superior Universitario)'                
             Item = Item1 + Item2
-            para = docx.add_paragraph(Item.format( request.academic_period, request.detail_cm['origin_program'], request.detail_cm['origin_institution']), style='List Number')
+            para = docx.add_paragraph(Item.format( request.academic_period, request.detail_cm['homologation'][-1], request.detail_cm['homologation'][-2]), style='List Number')
             para.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
             para.paragraph_format.space_after = Pt(0)
-            paragraph.paragraph_format.space_after = Pt(0)
             HCEMPRE.case_HOMOLOGACION_CONVALIDACION_EQUIVALENCIA_PREGRADO_TABLE(request, docx,'homologation')
-        elif request.detail_cm['equivalence']:
-            Item = 'equivaler'
-        elif request.detail_cm['recognition']:
-            Item = 'convalidar'
+        if 'equivalence' in request.detail_cm:
+            Item1 = 'Equivaler en el periodo académico {}, la(s) siguiente(s) asignatura(s) cursada(s)'
+            Item2 = ' en el programa {}, de la siguiente manera (Artículo 35 del Acuerdo 008 de 2008 del Consejo Superior Universitario)'                
+            Item = Item1 + Item2
+            para = docx.add_paragraph(Item.format( request.academic_period, request.detail_cm['equivalence'][-1], request.detail_cm['equivalence'][-2]), style='List Number')
+            para.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+            para.paragraph_format.space_after = Pt(0)
+            HCEMPRE.case_HOMOLOGACION_CONVALIDACION_EQUIVALENCIA_PREGRADO_TABLE(request, docx,'equivalence')
+        if 'recognition' in request.detail_cm:
+            Item1 = 'Convalidar en el periodo académico {}, la(s) siguiente(s) asignatura(s) cursada(s)'
+            Item2 = ' en el programa {} de la {}, de la siguiente manera'                
+            Item = Item1 + Item2
+            para = docx.add_paragraph(Item.format( request.academic_period, request.detail_cm['recognition'][-1], request.detail_cm['recognition'][-2]), style='List Number')
+            para.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+            para.paragraph_format.space_after = Pt(0)
+            HCEMPRE.case_HOMOLOGACION_CONVALIDACION_EQUIVALENCIA_PREGRADO_TABLE(request, docx,'recognition')
 
 
     @staticmethod
@@ -49,11 +61,12 @@ class HCEMPRE():
         B = 0
         C = 0
         cant_rows = 0
-        for i in range (0, len(request.detail_cm[case])):           
+        for i in range (0, len(request.detail_cm[case]) - 2 ):           
             if len(request.detail_cm[case][i]['origin_subject']) > len(request.detail_cm[case][i]['subject_out']):
                 cant_rows += len(request.detail_cm[case][i]['origin_subject'])
             else:
                 cant_rows += len(request.detail_cm[case][i]['subject_out'])
+
         table = docx.add_table(rows=cant_rows+3, cols=7, style='Table Grid')
         table.style.font.size = Pt(8)
         table.alignment = WD_ALIGN_PARAGRAPH.CENTER
@@ -65,7 +78,7 @@ class HCEMPRE():
         cellp.add_run('Asignaturas a {} en el plan de estudios de {}({})'.format(case_d[case], request.get_academic_program_display(), request.academic_program)).font.bold = True
         cellp = table.cell(1, 5).merge(table.cell(1, 6)).paragraphs[0]
         cellp.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        cellp.add_run('Asignaturas cursadas en el plan de estudios {} de la {}'.format(request.detail_cm['origin_program'], request.detail_cm['origin_institution'])).font.bold = True
+        cellp.add_run('Asignaturas cursadas en el plan de estudios {} de la {}'.format(request.detail_cm[case][-1], request.detail_cm[case][-2])).font.bold = True
         table.cell(2, 0).paragraphs[0].add_run('Código')
         table.cell(2, 1).paragraphs[0].add_run('Asignatura')
         table.cell(2, 2).paragraphs[0].add_run('C')
@@ -74,7 +87,7 @@ class HCEMPRE():
         table.cell(2, 5).paragraphs[0].add_run('Asignatura')
         table.cell(2, 6).paragraphs[0].add_run('Nota')
         row_a = 3
-        for i in range (0, len(request.detail_cm[case])):           
+        for i in range (0, len(request.detail_cm[case]) - 2):           
             if len(request.detail_cm[case][i]['origin_subject']) > len(request.detail_cm[case][i]['subject_out']):
                 row_m = row_a + len(request.detail_cm[case][i]['origin_subject']) - 1
                 cellp = table.cell(row_a, 0).merge(table.cell(row_m, 0)).paragraphs[0]
