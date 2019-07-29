5b9d99d25f4a5bbb1b20cb650ac13e4d0551206a
Laura Molina <ldmolinav@unal.edu.co>
2019-07-29 11:46:05 -0500

    table CGRUPXX

--- council_minutes/cm_cases/comp_cases/case_CGRUPXX.py
+++ council_minutes/cm_cases/comp_cases/case_CGRUPXX.py
@@ -1,5 +1,6 @@
 from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.table import WD_ALIGN_VERTICAL
 
 
 class CGRUPXX():
@@ -23,15 +24,35 @@ class CGRUPXX():
 
         table = docx.add_table(
             rows=len(request.detail_cm['subjects'])+1, cols=6)
+        
+        for column in table.columns:
+            for cell in column.cells:
+                cell.vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+                cell.paragraphs[0].alignment = WD_ALIGN_PARAGRAPH.CENTER
+
         table.style = 'Table Grid'
-        table.style.font.size = Pt(9)
+        table.style.font.size = Pt(8)
         table.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        table.columns[0].width = 2000000
-        table.columns[1].width = 800000
-        table.columns[2].width = 900000
-        table.columns[3].width = 700000
-        table.columns[4].width = 500000
-        table.columns[5].width = 500000
+        # table.columns[0].width = 200000
+        # table.columns[1].width = 800000
+        # table.columns[2].width = 900000
+        # table.columns[3].width = 700000
+        # table.columns[4].width = 500000
+        # table.columns[5].width = 500000
+
+        for cell in table.columns[0].cells:
+            cell.width = 2050000
+        for cell in table.columns[1].cells:
+            cell.width = 800000
+        for cell in table.columns[2].cells:
+            cell.width = 900000
+        for cell in table.columns[3].cells:
+            cell.width = 700000
+        for cell in table.columns[4].cells:
+            cell.width = 500000
+        for cell in table.columns[5].cells:
+            cell.width = 500000
+
         table.cell(0, 0).paragraphs[0].add_run('Asignatura').font.bold = True
         table.cell(0, 0).paragraphs[0] = WD_ALIGN_PARAGRAPH.CENTER
         table.cell(0, 1).paragraphs[0].add_run('Código').font.bold = True
