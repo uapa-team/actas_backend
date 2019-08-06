eea85f09ae0712460ce64b1b70c577aca68a6d33
Daniel Escobar <daescobarp@unal.edu.co>
2019-08-06 16:29:16 -0500

    CAIMPRE working propertly

--- council_minutes/cm_cases/comp_cases/case_simple.py
+++ council_minutes/cm_cases/comp_cases/case_simple.py
@@ -1099,7 +1099,64 @@ class simple():
     
     @staticmethod
     def case_CANCELACION_DE_ASIGNATURAS_CON_CARGA_INFERIOR_A_LA_MINIMA_PREGRADO(request, docx, redirected=False):
-        raise NotImplementedError
+        if redirected:
+            para = docx.paragraphs[-1]
+        else:
+            para = docx.add_paragraph()
+            para.add_run('El Consejo de Facultad ')
+            para.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        if request.approval_status == 'AP':
+            para.add_run('APRUEBA:').font.bold = True
+        else:
+            para.add_run('NO APRUEBA: ').font.bold = True
+        para.paragraph_format.space_after = Pt(0)
+        para = docx.add_paragraph(style='List Number')
+        para.add_run('Cursar el periodo académico ' + request.academic_period + ' con un número de ')
+        para.add_run('créditos inferior al mínimo exigido, porque ' + request.justification)
+        para.add_run('. (Artículo 10 del Acuerdo 008 de 2008 del Consejo Superior Universitario)')
+        para.paragraph_format.space_after = Pt(0)
+        para = docx.add_paragraph(style='List Number')
+        para.add_run('Cancelar la(s) siguiente(s) asignatura(s) inscrita(s) del periodo ')
+        para.add_run(request.academic_period + ', porque ' + request.justification)
+        para.add_run('. (Artículo 15 del Acuerdo 008 de 2008 del Consejo Superior Universitario)')
+        para.paragraph_format.space_after = Pt(0)
+        table = docx.add_table(
+            rows=len(request.detail_cm['subjects'])+1, cols=5)
+        table.style = 'Table Grid'
+        table.style.font.size = Pt(8)
+        table.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        table.columns[0].width = 750000
+        table.columns[1].width = 2300000
+        table.columns[2].width = 800000
+        table.columns[3].width = 800000
+        table.columns[4].width = 800000
+        for column in table.columns:
+            for cell in column.cells:
+                cell.vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+                cell.paragraphs[0].alignment = WD_ALIGN_PARAGRAPH.CENTER
+        for cell in table.columns[0].cells:
+            cell.width = 750000
+        for cell in table.columns[1].cells:
+            cell.width = 2300000
+        for cell in table.columns[2].cells:
+            cell.width = 800000
+        for cell in table.columns[3].cells:
+            cell.width = 800000
+        for cell in table.columns[4].cells:
+            cell.width = 800000
+        table.cell(0, 0).paragraphs[0].add_run('Código SIA').font.bold = True
+        table.cell(0, 1).paragraphs[0].add_run('Nombre Asignatura').font.bold = True
+        table.cell(0, 2).paragraphs[0].add_run('Grupo').font.bold = True
+        table.cell(0, 3).paragraphs[0].add_run('Tipología').font.bold = True
+        table.cell(0, 4).paragraphs[0].add_run('Créditos').font.bold = True
+        index = 0
+        for subject in request.detail_cm['subjects']:
+            table.cell(index+1, 0).paragraphs[0].add_run(subject['cod_sia'])
+            table.cell(index+1, 1).paragraphs[0].add_run(subject['name_asig'])
+            table.cell(index+1, 2).paragraphs[0].add_run(subject['group_asig'])
+            table.cell(index+1, 3).paragraphs[0].add_run(subject['tipo_asig'])
+            table.cell(index+1, 4).paragraphs[0].add_run(subject['creds_asig'])
+            index = index + 1
 
     @staticmethod
     def case_HOMOLOGACION_DE_ASIGNATURAS_INTERCAMBIO_ACADEMICO_INTERNACIONAL_PREGRADO(request, docx, redirected=False):
