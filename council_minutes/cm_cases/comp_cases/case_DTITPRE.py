fdf60ac5919dc0501927918b191a4774a78ba39e
Laura Molina <ldmolinav@unal.edu.co>
2019-08-14 12:33:31 -0500

    solve index out

--- council_minutes/cm_cases/comp_cases/case_DTITPRE.py
+++ council_minutes/cm_cases/comp_cases/case_DTITPRE.py
@@ -25,8 +25,9 @@ class DTITPRE():
 
     @staticmethod
     def case_DOBLE_TITULACION_PREGRADO(request, docx, redirected=False):
-        para = docx.paragraphs[-1]
-        if not redirected:
+        if redirected:
+            para = docx.paragraphs[-1]
+        else:
             para = docx.add_paragraph()
             para.add_run('El Consejo de Facultad ')
         para.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
