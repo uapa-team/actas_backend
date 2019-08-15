79d45636276ea69b439f0967d388898ee1f2e3b3
Daniel Escobar <daescobarp@unal.edu.co>
2019-08-15 10:59:29 -0500

    EBAPPRE-pre working propertly

--- council_minutes/pre_cm_cases/comp_cases/case_utils.py
+++ council_minutes/pre_cm_cases/comp_cases/case_utils.py
@@ -1,32 +1,39 @@
-from num2words import num2words
-from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Pt
-from ...models import Request
+
 
 def header(request, docx):
     para = docx.add_paragraph()
     para.add_run('Tipo de solicitud:\t{}\n'.format(request.get_type_display()))
-    para.add_run('Justificación:\t\t{}\n'.format(request['pre_cm']['justification']))
+    para.add_run('Justificación:\t\t{}\n'.format(
+        request['pre_cm']['justification']))
     para.add_run('Soportes:\t\t{}\n'.format(request['pre_cm']['supports']))
-    para.add_run('Fecha radicación:\t{}\n'.format(request['date']))
+    para.add_run('Fecha radicación:\t{}'.format(request['date']))
+    para.paragraph_format.space_after = Pt(0)
+
 
 def table_general_data():
     raise NotImplementedError
 
+
 def table_subjects():
     raise NotImplementedError
+
 
 def table_english():
     raise NotImplementedError
 
+
 def table_approvals():
     raise NotImplementedError
+
 
 def table_credits_summary():
     raise NotImplementedError
 
+
 def table_recommend():
     raise NotImplementedError
+
 
 def table_change_typology():
     raise NotImplementedError
