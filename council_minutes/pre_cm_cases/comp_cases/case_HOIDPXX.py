512b0b5048d478b5583cf411c320e20d618f8e05
Toño <ajsuarezf@unal.edu.co>
2019-08-20 16:33:15 -0500

    Advancing!

--- council_minutes/pre_cm_cases/comp_cases/case_HOIDPXX.py
+++ council_minutes/pre_cm_cases/comp_cases/case_HOIDPXX.py
@@ -7,4 +7,26 @@ class HOIDPXX():
 
     @staticmethod
     def case_HOMOLOGACION_INGLES(request, docx, redirected=False):
-        raise NotImplementedError
+        case_HOMOLOGACION_INGLES_Analysis(request, docx)
+        case_HOMOLOGACION_INGLES_Answers(request, docx)
+
+    @staticmethod
+    def case_HOMOLOGACION_INGLES_Analysis(request):
+        para = docx.add_paragraph()
+        para.add_run('Analisis:')
+        analysis_para = docx.add_paragraph()
+        analysis_para.paragraph_format.left_indent = Pt(36)
+        analysis_str = '1. Obtuvo un puntaje de {} en el examen {}'
+        analysis = analysis_str.format(
+            request['detail_cm']['grade_got'], request['detail_cm']['institution'])
+        analysis_para.add_run(analysis)
+
+    @staticmethod
+    def case_HOMOLOGACION_INGLES_Answers(request):
+        para = docx.add_paragraph()
+        para.add_run('Concepto:')
+        answer_para = docx.add_paragraph()
+        answer_para.paragraph_format.left_indent = Pt(36)
+        answer_1 = 'El Comité Asesor recomienda al Consejo de Facultad {} homologar,' + \
+            ' en el periodo académico {}, el requisito de idioma inglés por obtener' + \
+            ' un puntaje/calificación de {} en el examen {}, siendo {} el mínimo exigido. '
