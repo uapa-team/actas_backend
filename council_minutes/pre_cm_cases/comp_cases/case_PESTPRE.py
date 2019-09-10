e32ad5abf28178d6463f3c0d0ef6dcf30554e465
Toño <ajsuarezf@unal.edu.co>
2019-09-10 10:39:13 -0500

    Adding basis for case

--- council_minutes/pre_cm_cases/comp_cases/case_PESTPRE.py
+++ council_minutes/pre_cm_cases/comp_cases/case_PESTPRE.py
@@ -5,4 +5,29 @@ class PESTPRE():
 
     @staticmethod
     def case_PRACTICA_ESTUDIANTIL_PREGRADO(request, docx, redirected=False):
-       raise NotImplementedError
+        PESTPRE.case_PRACTICA_ESTUDIANTIL_PREGRADO_Analysis(request, docx)
+        PESTPRE.case_PRACTICA_ESTUDIANTIL_PREGRADO_Answers(request, docx)
+
+    @staticmethod
+    def case_PRACTICA_ESTUDIANTIL_PREGRADO_Analysis(request, docx):
+        pass
+
+    @staticmethod
+    def case_PRACTICA_ESTUDIANTIL_PREGRADO_Analysis_1(request, docx):
+        pass
+
+    @staticmethod
+    def case_PRACTICA_ESTUDIANTIL_PREGRADO_Analysis_2(request, docx):
+        pass
+
+    @staticmethod
+    def case_PRACTICA_ESTUDIANTIL_PREGRADO_Analysis_3(request, docx):
+        pass
+
+    @staticmethod
+    def case_PRACTICA_ESTUDIANTIL_PREGRADO_Analysis_4(request, docx):
+        pass
+
+    @staticmethod
+    def case_PRACTICA_ESTUDIANTIL_PREGRADO_Answers(request, docx):
+        pass
