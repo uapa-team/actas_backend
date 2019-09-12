9b2c5ba6bcb27e56dd1c56c1d3735251c26f35eb
Fortich <ajsuarezf@unal.edu.co>
2019-09-12 16:22:00 -0500

    Finished case

--- council_minutes/pre_cm_cases/comp_cases/case_HOIDPXX.py
+++ council_minutes/pre_cm_cases/comp_cases/case_HOIDPXX.py
@@ -7,8 +7,8 @@ class HOIDPXX():
 
     @staticmethod
     def case_HOMOLOGACION_INGLES(request, docx, redirected=False):
-        case_HOMOLOGACION_INGLES_Analysis(request, docx)
-        case_HOMOLOGACION_INGLES_Answers(request, docx)
+        HOIDPXX.case_HOMOLOGACION_INGLES_Analysis(request, docx)
+        HOIDPXX.case_HOMOLOGACION_INGLES_Answers(request, docx)
 
     @staticmethod
     def case_HOMOLOGACION_INGLES_Analysis(request, docx):
@@ -24,9 +24,14 @@ class HOIDPXX():
     @staticmethod
     def case_HOMOLOGACION_INGLES_Answers(request, docx):
         para = docx.add_paragraph()
-        para.add_run('Concepto:')
+        run = para.add_run('Concepto: ')
+        run.font.bold = True
         answer_para = docx.add_paragraph()
         answer_para.paragraph_format.left_indent = Pt(36)
-        answer_1 = 'El Comité Asesor recomienda al Consejo de Facultad {} homologar,' + \
+        answer_para.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        answer_1 = 'El Comité Asesor recomienda al Consejo de Facultad{} homologar,' + \
             ' en el periodo académico {}, el requisito de idioma inglés por obtener' + \
             ' un puntaje/calificación de {} en el examen {}, siendo {} el mínimo exigido. '
+        answer_para.add_run(answer_1.format('', request['academic_period'], 
+            request['detail_cm']['grade_got'], request['detail_cm']['institution'],
+            request['detail_cm']['min_grade']))
