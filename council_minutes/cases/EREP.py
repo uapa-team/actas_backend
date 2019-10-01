e0cd47024c186a8ee3fb72b232543763c23391ea
Daniel Escobar <daescobarp@unal.edu.co>
2019-10-01 15:20:32 -0500

    advanced

--- council_minutes/cases/EREP.py
+++ council_minutes/cases/EREP.py
@@ -9,3 +9,18 @@ from .case_utils import add_hyperlink, table_subjects
 class EREP(Request):
 
     full_name = 'Expedición de recibo'
+
+    date = DateField(display='Fecha límite de pago')
+
+    str_ap = 'APRUEBA'
+    str_na = 'NO APRUEBA'
+    str_analysis = 'Analisis'
+    str_answer = 'Concepto'
+    str_regulation_1 = 'Resolución 051 de 2003 del Consejo Superior Universitario'
+    str_regulation_1_link = 'http://www.legal.unal.edu.co/rlunal/home/doc.jsp?d_i=35163'
+
+    str_cm_1 = 'El Consejo de Facultad'
+    str_cm_2 = 'presentar con concepto positivo al Comité de Matrículas de la Sede Bogotá, ' + \
+        'la expedición de un único recibo correspondiente a los derechos académicos y ' + \
+        'administrativos para el periodo académico {} y se le concede como fecha de pago el ' + \
+        '{}, teniendo en cuenta el estado de pago por parte de {}.'
