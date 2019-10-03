91922141f6313eccdcb6d24c0ac2163faa07aa74
Daniel Escobar <daescobarp@unal.edu.co>
2019-10-03 13:04:04 -0500

    EREP working 100% final final

--- council_minutes/cases/EREP.py
+++ council_minutes/cases/EREP.py
@@ -2,7 +2,7 @@ from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from mongoengine import DateField, BooleanField
 from ..models import Request
-from .case_utils import string_to_date
+from .case_utils import string_to_date, add_analysis_paragraph
 
 
 class EREP(Request):
@@ -10,16 +10,15 @@ class EREP(Request):
     full_name = 'Expedición de recibo'
 
     ah_active = BooleanField(
-        required=True, display='¿Tiene active la historia académica?')
+        required=True, display='¿Tiene activa la historia académica?')
     payment_date = DateField(display='Fecha límite de pago')
 
     regulation_list = ['051|2003|CSU']  # List of regulations
 
-    str_cm_1 = 'presentar con concepto positivo al Comité de Matrículas de la Sede Bogotá, ' + \
-        'la expedición de un único recibo correspondiente a los derechos académicos y ' + \
-        'administrativos para el periodo académico {}'
-    str_cm_2 = ' y se le concede como fecha de pago el {} ({}), teniendo en cuenta el ' + \
-        'estado de pago por parte de {}.'
+    str_cm = ['presentar con concepto positivo al Comité de Matrículas de la Sede Bogotá, ' +
+              'la expedición de un único recibo correspondiente a los derechos académicos y ' +
+              'administrativos para el periodo académico {}', ' y se le concede como fecha de ' +
+              'pago el {} ({}), teniendo en cuenta el estado de pago por parte de {}.']
 
     list_analysis = ['El estudiante {}tiene la historia académica activa.']
 
@@ -34,7 +33,7 @@ class EREP(Request):
         paragraph.add_run(
             # pylint: disable=no-member
             self.get_approval_status_display().upper() + ' ').font.bold = True
-        paragraph.add_run(self.str_cm_1.format(self.academic_period))
+        paragraph.add_run(self.str_cm[0].format(self.academic_period))
         if self.is_affirmative_response_approval_status():
             self.cm_af(paragraph)
         else:
@@ -53,14 +52,14 @@ class EREP(Request):
         paragraph.add_run(
             # pylint: disable=no-member
             self.get_advisor_response_display().upper()).font.bold = True
-        paragraph.add_run(' ' + self.str_cm_1.format(self.academic_period))
+        paragraph.add_run(' ' + self.str_cm[0].format(self.academic_period))
         if self.is_affirmative_response_advisor_response():
             self.pcm_answers_af(paragraph)
         else:
             self.pcm_answers_ng(paragraph)
 
     def cm_af(self, paragraph):
-        paragraph.add_run(self.str_cm_2.format(
+        paragraph.add_run(self.str_cm[1].format(
             self.payment_date, string_to_date(str(self.payment_date)), self.student_name))
 
     def cm_ng(self, paragraph):
@@ -72,21 +71,12 @@ class EREP(Request):
         else:
             active = 'no '
         self.list_analysis[0] = self.list_analysis[0].format(active)
-        paragraph = docx.add_paragraph()
-        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-        paragraph.paragraph_format.space_after = Pt(0)
-        paragraph.add_run(self.str_analysis + ': ').font.bold = True
-        for extra in self.extra_analysis:
-            self.list_analysis.append(extra)
-        for analysis in self.list_analysis:
-            paragraph = docx.add_paragraph()
-            paragraph.style = 'List Bullet'
-            paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-            paragraph.paragraph_format.space_after = Pt(0)
-            paragraph.add_run(analysis)
+        for extra_a in self.extra_analysis:
+            self.list_analysis.append(extra_a)
+        add_analysis_paragraph(docx, self.list_analysis)
 
     def pcm_answers_af(self, paragraph):
-        paragraph.add_run(self.str_cm_2.format(
+        paragraph.add_run(self.str_cm[1].format(
             self.payment_date, string_to_date(str(self.payment_date)), self.student_name))
 
     def pcm_answers_ng(self, paragraph):
