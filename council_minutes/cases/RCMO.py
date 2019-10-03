0ef7c9cbc7c17c89167daf5392bdb24a9c34a04d
Fortich <ajsuarezf@unal.edu.co>
2019-10-03 12:15:33 -0500

    Missing pcm and pcm_answer

--- council_minutes/cases/RCMO.py
+++ council_minutes/cases/RCMO.py
@@ -1,9 +1,6 @@
-from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
-from docx.enum.table import WD_ALIGN_VERTICAL
-from mongoengine import DynamicDocument, DateField, StringField, ListField, IntField, FloatField, EmbeddedDocumentListField
-from ..models import Request, Subject
-from .case_utils import add_hyperlink, table_subjects
+from mongoengine import StringField
+from ..models import Request
 
 
 class RCMO(Request):
@@ -13,8 +10,8 @@ class RCMO(Request):
     CALIFICATION_AP = 'AP'
     CALIFICATION_NA = 'NA'
     CALIFICATION_CHOICES = (
-        (CALIFICATION_AP, 'Aprueba'),
-        (CALIFICATION_NA, 'No aprueba')
+        (CALIFICATION_AP, 'Aprobada'),
+        (CALIFICATION_NA, 'No aprobada')
     )
 
     calification = StringField(
@@ -23,12 +20,7 @@ class RCMO(Request):
     subject_name = StringField(display='Nombre asignatura')
     subject_period = StringField(display='Periodo asignatura')
 
-    str_ap = 'APRUEBA'
-    str_na = 'NO APRUEBA'
-    str_analysis = 'Analisis'
-    str_answer = 'Concepto'
-    str_ap_p = 'aprobada'
-    str_na_p = 'no aprobada'
+    regulation_list = ['008|2008|CSU']  # List of regulations
 
     str_cm_1 = 'El Consejo de Facultad'
     str_cm_2 = 'calificar {} la asignatura {} ({}) en el periodo {}.'
@@ -40,17 +32,20 @@ class RCMO(Request):
         self.cm_answer(paragraph)
 
     def cm_answer(self, paragraph):
-        if self.approval_status == self.APPROVAL_STATUS_APRUEBA:
-            paragraph.add_run(self.str_ap).font.bold = True
-        elif self.approval_status == self.APPROVAL_STATUS_NO_APRUEBA:
-            paragraph.add_run(self.str_na).font.bold = True
-        else:
-            raise AssertionError(
-                'Council minute answer, must have approval_status AP or NA')
+        paragraph.add_run(
+            # pylint: disable=no-member
+            self.get_approval_status_display().upper()).font.bold = True
         paragraph.add_run(' ')
-        if self.calification == self.CALIFICATION_AP:
-            paragraph.add_run(self.str_cm_2.format(
-                self.str_ap_p, self.subject_code, self.subject_code, self.subject_period))
-        elif self.calification == self.CALIFICATION_NA:
-            paragraph.add_run(self.str_cm_2.format(
-                self.str_na_p, self.subject_code, self.subject_code, self.subject_period))
+        paragraph.add_run(self.str_cm_2.format(
+            # pylint: disable=no-member
+            self.get_calification_display().lower(),
+            self.subject_name,
+            self.subject_code,
+            self.subject_period))
+
+    def pcm(self, docx):
+        self.pcm_analysis(docx)
+        self.pcm_answer(docx)
+
+    def pcm_answer(self, docx):
+        pass
