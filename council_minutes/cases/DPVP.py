9ed91e66947678e58bcb42fbf8be174678e9860b
Angel Corredor <adcorredorm@unal.edu.co>
2019-10-12 17:03:04 -0500

    DPVP case created

---
+++ council_minutes/cases/DPVP.py
@@ -0,0 +1,43 @@
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Pt
+from mongoengine import StringField, FloatField
+from ..models import Request
+from .case_utils import add_analysis_paragraph
+
+
+class DPVP(Request):
+
+    full_name = 'Devolución proporcional del valor pagado por concepto de derechos de matrícula'
+
+    ### Fields ###
+
+    regulation_list = []
+
+    str_cm = []
+    str_pcm = []
+
+    def cm(self, docx):
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.paragraph_format.space_after = Pt(0)
+        self.cm_answer(paragraph)
+
+    def cm_answer(self, paragraph):
+        paragraph.add_run(self.str_council_header + ' ')
+        paragraph.add_run(
+            # pylint: disable=no-member
+            self.get_approval_status_display().upper() + ' ').font.bold = True
+
+    def pcm(self, docx):
+        add_analysis_paragraph(docx, self.extra_analysis)
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.paragraph_format.space_after = Pt(0)
+        self.pcm_answer(paragraph)
+
+    def pcm_answer(self, paragraph):
+        paragraph.add_run(self.str_answer + ' ').font.bold = True
+        paragraph.add_run(self.str_comittee_header + ' ')
+        paragraph.add_run(
+            # pylint: disable=no-member
+            self.get_advisor_response_display().upper() + ' ').font.bold = True
