4ebaa7946b595c790b575e3785c34e27ce9634aa
Fortich <ajsuarezf@unal.edu.co>
2019-10-16 10:05:25 -0500

    Finished PCM

--- council_minutes/cases/AAUT.py
+++ council_minutes/cases/AAUT.py
@@ -11,15 +11,34 @@ class AAUT(Request):
 
     regulation_list = ['008|2008|CSU', '070|2009|CA']  # List of regulations
 
+    PROFILE_INVE = 'I'
+    PROFILE_PROF = 'P'
+    PROFILE_CHOICES = (
+        (PROFILE_INVE, 'Investigación'),
+        (PROFILE_PROF, 'Profundización'),
+    )
+
+    academic_profile = StringField(
+        default='I', choices=PROFILE_CHOICES, display='Perfil de programa curricular')
+    places_resolution = StringField(
+        display='Resolución de cupos para admisión automática.')
+    last_academic_program = StringField(
+        min_length=4, max_length=4, choices=Request.PLAN_CHOICES,
+        required=True, display='Anterior Programa Académico')
+    admission_academic_period = StringField(max_length=10,
+                                            display='Periodo de ingreso al nuevo programa')
+
     str_cm = [
         'la admisión automática al programa {} ({}), a partir del periodo académico {}.',
         'Debido a que {}justifica debidamente la solicitud.'
     ]
 
     str_pcm = [
-        'El estudiante completó plan de estudios en {}.',
+        'El estudiante completó plan de estudios en el plan curricular {} ({}).',
         'Cupo de admisión automática en resolución {}.',
-        'Solicita admisión al plan de estudios {} - perfil de {}.',
+        'Solicita admisión al plan de estudios {} ({}) - perfil de {}.',
+        'admisión automática al programa {} ({}) en el plan de estudios de {} a partir del period' +
+        'o académico {}.'
     ]
 
     def cm(self, docx):
@@ -39,22 +58,50 @@ class AAUT(Request):
             # pylint: disable=no-member
             self.get_approval_status_display().upper() + ' ').font.bold = True
         paragraph.add_run(self.str_cm[0].format(
+            # pylint: disable=no-member
             self.get_academic_program_display(),
             self.academic_program,
             self.academic_period,
             '' if self.is_affirmative_response_approval_status() else 'no ') + ' ')
 
     def pcm(self, docx):
-        self.pcm_analysis(self, docx)
+        self.pcm_analysis(docx)
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.paragraph_format.space_after = Pt(0)
+        paragraph.add_run(self.str_answer + ': ').bold = True
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.paragraph_format.space_after = Pt(0)
+        paragraph.add_run(self.str_comittee_header + ' ')
+        self.pcm_answer(paragraph)
 
     def pcm_analysis(self, docx):
         analysis_list = []
         analysis_list += [self.str_pcm[0].format(
-            self.advance, self.enrolled_academic_periods, self.papa)]
-        analysis_list += [self.str_pcm[1].format(self.available_credits)]
-        analysis_list += self.pcm_analysis_subject_list()
+            # pylint: disable=no-member
+            self.get_last_academic_program_display(),
+            self.last_academic_program)]
+        analysis_list += [self.str_pcm[1].format(self.places_resolution)]
+        analysis_list += [self.str_pcm[2].format(
+            # pylint: disable=no-member
+            self.get_academic_program_display(),
+            self.academic_program,
+            self.get_academic_profile_display())]
         analysis_list += self.extra_analysis
         add_analysis_paragraph(docx, analysis_list)
 
     def pcm_answer(self, paragraph):
-        pass
+        paragraph.add_run(
+            # pylint: disable=no-member
+            self.get_advisor_response_display().upper()).font.bold = True
+        paragraph.add_run(' ' + self.str_pcm[3].format(
+            # pylint: disable=no-member
+            self.get_academic_program_display(),
+            self.academic_program,
+            self.get_last_academic_program_display(),
+            self.last_academic_program,
+            self.admission_academic_period
+        ))
+        paragraph.add_run(' ({}. {}).'.format(
+            self.regulations['070|2009|CA'][0], self.regulations['008|2008|CSU'][0]))
