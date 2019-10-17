80cd400cb95ba69534b0e590f319df1a89a89e22
Fortich <ajsuarezf@unal.edu.co>
2019-10-17 11:37:58 -0500

    Finished CM

--- council_minutes/cases/CPER.py
+++ council_minutes/cases/CPER.py
@@ -5,18 +5,40 @@ from ..models import Request, Subject
 from .case_utils import table_subjects, add_analysis_paragraph, num_to_month
 
 
-class TGRA(Request):
+class CPER(Request):
 
     full_name = 'Cambio de perfil'
 
-    origin_profile = StringField(display='Nodo origen')
-    destin_node = StringField(display='Nodo destino')
+    origin_profile = StringField(
+        choices=Request.PROFILE_CHOICES, display='Perfil origen')
+    destin_profile = StringField(
+        choices=Request.PROFILE_CHOICES, display='Perfil destino')
+
+    str_cm = [
+        'Traslado intrafacultad del estudiante de {} ({}) en el perfil de {} al plan de estudios ' +
+        '{} ({}) en el perfil de {}, debido a que {}justifica adecuadamente su solicitud.'
+    ]
 
     def cm(self, docx):
-        pass
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.paragraph_format.space_after = Pt(0)
+        paragraph.add_run(self.str_council_header + ' ')
+        self.cm_answer(paragraph)
 
     def cm_answer(self, paragraph):
-        pass
+        paragraph.add_run(
+            # pylint: disable=no-member
+            self.get_approval_status_display().upper() + ' ').font.bold = True
+        paragraph.add_run(self.str_cm[0].format(
+            # pylint: disable=no-member
+            self.get_academic_program_display(),
+            self.academic_program,
+            self.get_origin_profile_display(),
+            self.get_academic_program_display(),
+            self.academic_program,
+            self.get_destin_profile_display(),
+            '' if self.is_affirmative_response_approval_status else 'no'))
 
     def pcm(self, docx):
         pass
