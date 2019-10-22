f9ce5ea255dd440581dd32a0137f8ddca1925454
Fortich <ajsuarezf@unal.edu.co>
2019-10-22 11:48:25 -0500

    Fixed CM, finished PCM for CAIM

--- council_minutes/cases/CAIM.py
+++ council_minutes/cases/CAIM.py
@@ -9,6 +9,11 @@ class CAIM(Request):
 
     full_name = 'Cancelación de asignaturas con carga inferior a la mínima'
 
+    percentaje = FloatField(display='Porcentaje de avance de carrrera')
+    enrollments = IntField(display='Número de matrículas')
+    gpa = FloatField(display='P.A.P.A.')
+    available_credits = IntField(display='Créditos disponibles')
+    remaining_credits = IntField(display='Créditos restantes')
     subjects = EmbeddedDocumentListField(
         Subject, required=True, display='Asignaturas')
 
@@ -16,7 +21,7 @@ class CAIM(Request):
 
     str_cm = [
         'cursar el periodo académico {} con un número de créditos inferior al mínimo exigido, ',
-        'Cancelar la(s) siguiente(s) asignatura(s) inscrita(s) del periodo {}.',
+        'cancelar la(s) siguiente(s) asignatura(s) inscrita(s) del periodo {}.',
         'debido a que {}realiza debidamente la solicitud.',
         '(Artículo 10 del {}).',
         '(Artículo 15 del {}).'
@@ -24,13 +29,13 @@ class CAIM(Request):
 
     str_pcm = [
         'SIA: Porcentaje de avance en el plan: {}%. Número de matrículas: {}. P.A.P.A.: {}.',
-        ' SIA: Créditos disponibles: {}. ',
+        'SIA: Créditos disponibles: {}. ',
         'SIA: Al aprobar la cancelación de la(s) asignatura(s) solicitada(s) el estudiante ' +
         'quedaría con {} créditos inscritos.'
     ]
 
     def cm(self, docx):
-        if self.is_affirmative_response_advisor_response():
+        if self.is_affirmative_response_approval_status():
             self.cm_ap(docx)
         else:
             self.cm_na(docx)
@@ -44,15 +49,16 @@ class CAIM(Request):
         paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
         paragraph.style = 'List Bullet'
         self.cm_answer(paragraph)
-        paragraph.add_run(self.str_cm[2].format(
-            '' if self.is_affirmative_response_approval_status() else 'no '
-        ) + ' ')
+        paragraph.add_run(self.str_cm[2].format('') + ' ')
         paragraph.add_run(self.str_cm[3].format(
             Request.regulations[self.regulation_list[0]][0]))
         paragraph = docx.add_paragraph()
         paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
         paragraph.style = 'List Bullet'
-        paragraph.add_run(self.str_cm[1].format(
+        paragraph.add_run(
+            # pylint: disable=no-member
+            self.get_approval_status_display().upper()).font.bold = True
+        paragraph.add_run(' ' + self.str_cm[1].format(
             self.academic_period) + ' ')
         paragraph.add_run(self.str_cm[4].format(
             Request.regulations[self.regulation_list[0]][0]))
@@ -64,9 +70,7 @@ class CAIM(Request):
         paragraph.paragraph_format.space_after = Pt(0)
         paragraph.add_run(self.str_council_header + ' ')
         self.cm_answer(paragraph)
-        paragraph.add_run(self.str_cm[2].format(
-            '' if self.is_affirmative_response_approval_status() else 'no '
-        ) + ' ')
+        paragraph.add_run(self.str_cm[2].format('no ') + ' ')
         paragraph.add_run(self.str_cm[3].format(
             Request.regulations[self.regulation_list[0]][0]))
 
@@ -79,22 +83,64 @@ class CAIM(Request):
 
     def pcm(self, docx):
         self.pcm_analysis(docx)
+        if self.is_affirmative_response_advisor_response():
+            self.pcm_ap(docx)
+        else:
+            self.pcm_na(docx)
+
+    def pcm_ap(self, docx):
         paragraph = docx.add_paragraph()
         paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
         paragraph.paragraph_format.space_after = Pt(0)
-        paragraph.add_run(self.str_answer + ': ').bold = True
+        paragraph.add_run(self.str_comittee_header + ':')
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.style = 'List Bullet'
         self.pcm_answer(paragraph)
+        paragraph.add_run(self.str_cm[2].format('') + ' ')
+        paragraph.add_run(self.str_cm[3].format(
+            Request.regulations[self.regulation_list[0]][0]))
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.style = 'List Bullet'
+        paragraph.add_run(
+            # pylint: disable=no-member
+            self.get_advisor_response_display().upper()).font.bold = True
+        paragraph.add_run(' ' + self.str_cm[1].format(
+            self.academic_period) + ' ')
+        paragraph.add_run(self.str_cm[4].format(
+            Request.regulations[self.regulation_list[0]][0]))
         table_subjects(docx, Subject.subjects_to_array(self.subjects))
+
+    def pcm_na(self, docx):
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.paragraph_format.space_after = Pt(0)
+        paragraph.add_run(self.str_comittee_header + ' ')
+        self.pcm_answer(paragraph)
+        paragraph.add_run(self.str_cm[2].format('no ') + ' ')
+        paragraph.add_run(self.str_cm[3].format(
+            Request.regulations[self.regulation_list[0]][0]))
 
     def pcm_answer(self, paragraph):
         paragraph.add_run(
             # pylint: disable=no-member
-            self.get_approval_status_display().upper() + ' ').font.bold = True
+            self.get_advisor_response_display().upper() + ' ').font.bold = True
         # pylint: disable=no-member
         paragraph.add_run(self.str_cm[0].format(self.academic_period))
 
     def pcm_analysis(self, docx):
         analysis_list = []
-        analysis_list += [self.str_pcm[0].format()]
+        analysis_list += [self.str_pcm[0].format(
+            self.percentaje,
+            self.enrollments,
+            self.gpa
+        )]
+        analysis_list += [self.str_pcm[1].format(
+            self.available_credits
+        )]
+        analysis_list += [self.str_pcm[2].format(
+            self.remaining_credits
+        )]
         analysis_list += self.extra_analysis
         add_analysis_paragraph(docx, analysis_list)
