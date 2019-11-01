b2dc8897c16f38c6a7e8a8d2df5975c20787e90e
Fortich <ajsuarezf@unal.edu.co>
2019-11-01 12:38:07 -0500

    Added test example

--- council_minutes/tests.py
+++ council_minutes/tests.py
@@ -1,3 +1,13 @@
+from docx import Document
 from django.test import TestCase
+from council_minutes.models import Subject, Request, Professor
 
-# Create your tests here.
+
+class AAUTestCase(TestCase):
+
+    def test_can_create_cm(self):
+        """AAUT can write on CM correctly"""
+        # pylint: disable=no-member
+        document = Document()
+        request = Request.objects.get(id='5da67f49d76fda9c14c66e42')
+        request.cm(document)
