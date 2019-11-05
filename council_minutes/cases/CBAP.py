f43812e74825c42312a3175a43bbbd6ebd5d8d20
Nicolás Gómez <nigomezgu@unal.edu.co>
2019-11-05 11:38:13 -0500

    CBAP PCM added.

--- council_minutes/cases/CBAP.py
+++ council_minutes/cases/CBAP.py
@@ -39,3 +39,24 @@ class CBAP(Request):
         paragraph.add_run(self.str_cm[3])
         paragraph.add_run(
             self.str_cm[4] + self.regulations['026|2012|CSU'][0] + ').')
+
+    # PCM for CBAP not found. Using the same of cm.
+    def pcm(self, docx):
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.paragraph_format.space_after = Pt(0)
+        self.pcm_answer(paragraph)
+
+    def pcm_answer(self, paragraph):
+        paragraph.add_run(self.str_council_header + ' ')
+        paragraph.add_run(
+            # pylint: disable=no-member
+            self.get_approval_status_display().upper() + ' ').font.bold = True
+        paragraph.add_run(self.str_cm[0] + str(self.credits) + self.str_cm[1])
+        paragraph.add_run(self.get_academic_program_display())
+        if self.approval_status != 'CAP':
+            paragraph.add_run(self.str_cm[2] + self.student_justification)
+            return
+        paragraph.add_run(self.str_cm[3])
+        paragraph.add_run(
+            self.str_cm[4] + self.regulations['026|2012|CSU'][0] + ').')
