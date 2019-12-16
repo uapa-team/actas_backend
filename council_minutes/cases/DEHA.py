3d6aef3c48c09f21b7bd95ef0a8784b848d6387e
Fortich <ajsuarezf@unal.edu.co>
2019-12-16 12:13:47 -0500

    Adding DEHA :hammer:

---
+++ council_minutes/cases/DEHA.py
@@ -0,0 +1,61 @@
+import datetime
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Pt
+from mongoengine import StringField, DateField
+from ..models import Request
+from .case_utils import add_analysis_paragraph
+
+
+class DEHA(Request):
+
+    full_name = 'Permiso Académico'
+
+    reason_permision = StringField(required=True, display='Razón del permiso académico',
+        default='Razón del permiso académico')
+    from_date = DateField(
+        required=True, display='Fecha de inicio del permiso', default=datetime.date.today)
+    to_date = DateField(
+        required=True, display='Fecha de fin del permiso', default=datetime.date.today)
+
+    str_cm = [
+        'otorgar permiso académico. Desde la fecha {} de {} del {}, a la fecha {} de {} del {}.',
+    ]
+
+    regulation_list = ['070|2019|CSU']  # List of regulations
+
+    def cm(self, docx):
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.paragraph_format.space_after = Pt(0)
+        self.cm_answer(paragraph)
+        paragraph.add_run(' ({}). '.format(
+            self.regulations[self.regulation_list[0]][0]))
+
+    def cm_answer(self, paragraph):
+        paragraph.add_run(self.str_council_header + ' ')
+        paragraph.add_run(
+            # pylint: disable=no-member
+            self.get_approval_status_display().upper() + ' ').font.bold = True
+        paragraph.add_run(self.str_cm[0].format(from_date, to_date) + ' ')
+        paragraph.add_run(reason_permision + ' ')
+
+    def pcm(self, docx):
+        self.pcm_analysis(docx)
+        paragraph = docx.add_paragraph()
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+        paragraph.paragraph_format.space_after = Pt(0)
+        paragraph.add_run(self.str_answer + ': ').bold = True
+        self.pcm_answer(paragraph)
+
+    def pcm_analysis(self, docx):
+        analysis_list = []
+        analysis_list += self.extra_analysis
+        add_analysis_paragraph(docx, analysis_list)
+
+    def pcm_answer(self, paragraph):
+        paragraph.add_run(self.str_comittee_header + ' ')
+        paragraph.add_run(
+            # pylint: disable=no-member
+            self.get_advisor_response_display().upper() + ' ').font.bold = True
+        paragraph.add_run(self.str_cm[0].format(from_date, to_date) + ' ')
+        paragraph.add_run(reason_permision + ' ')
