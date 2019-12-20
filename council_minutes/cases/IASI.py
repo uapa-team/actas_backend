9b10713c92c0647e118f15c6d45d287442caa1d6
Fortich <ajsuarezf@unal.edu.co>
2019-12-20 11:19:47 -0500

    IASI now can be partially approved

--- council_minutes/cases/IASI.py
+++ council_minutes/cases/IASI.py
@@ -8,6 +8,7 @@ from .case_utils import table_subjects, add_analysis_paragraph
 class IASISubject(Subject):
     offered = BooleanField(display='Ofrecida para el plan de estudios')
     overlap = BooleanField(display='Materia cruzada')
+    approved = BooleanField(display='Aprobado inscribir materia')
 
 
 class IASI(Request):
@@ -30,11 +31,26 @@ class IASI(Request):
     regulation_list = ['008|2008|CSU']  # List of regulations
 
     def cm(self, docx):
-        paragraph = docx.add_paragraph()
-        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-        paragraph.paragraph_format.space_after = Pt(0)
-        self.cm_answer(paragraph)
-        table_subjects(docx, Subject.subjects_to_array(self.subjects))
+        sapproved = []
+        snotapproved = []
+        for subject in self.subjects:
+            if subject.approved:
+                sapproved.append(subject)
+            else:
+                snotapproved.append(subject)
+        if len(sapproved) > 0:
+            paragraph = docx.add_paragraph()
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+            paragraph.paragraph_format.space_after = Pt(0)
+            self.cm_answer_approved(paragraph)
+            table_subjects(docx, Subject.subjects_to_array(snotapproved))
+        if len(snotapproved) > 0:
+            paragraph = docx.add_paragraph()
+            paragraph = docx.add_paragraph()
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+            paragraph.paragraph_format.space_after = Pt(0)
+            self.cm_answer_not_approved(paragraph)
+            table_subjects(docx, Subject.subjects_to_array(snotapproved))
 
     def cm_answer(self, paragraph):
         paragraph.add_run(self.str_council_header + ' ')
@@ -49,16 +65,57 @@ class IASI(Request):
             '' if self.is_affirmative_response_approval_status() else 'no '))
         paragraph.add_run('({}).'.format(self.regulations['008|2008|CSU'][0]))
 
+    def cm_answer_approved(self, paragraph):
+        paragraph.add_run(self.str_council_header + ' ')
+        paragraph.add_run(
+            # pylint: disable=no-member
+            'APRUEBA ').font.bold = True
+        paragraph.add_run(self.str_cm[0].format(
+            # pylint: disable=no-member
+            self.get_academic_program_display(),
+            self.academic_program,
+            self.academic_period,
+            ''))
+        paragraph.add_run('({}).'.format(self.regulations['008|2008|CSU'][0]))
+
+    def cm_answer_not_approved(self, paragraph):
+        paragraph.add_run(self.str_council_header + ' ')
+        paragraph.add_run(
+            # pylint: disable=no-member
+            'NO APRUEBA ').font.bold = True
+        paragraph.add_run(self.str_cm[0].format(
+            # pylint: disable=no-member
+            self.get_academic_program_display(),
+            self.academic_program,
+            self.academic_period,
+            'no '))
+        paragraph.add_run('({}).'.format(self.regulations['008|2008|CSU'][0]))
+
     def pcm(self, docx):
         self.pcm_analysis(docx)
         paragraph = docx.add_paragraph()
         paragraph.add_run(self.str_answer + ': ').bold = True
-        paragraph = docx.add_paragraph()
-        paragraph.add_run(self.str_comittee_header)
-        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-        paragraph.paragraph_format.space_after = Pt(0)
-        self.pcm_answer(paragraph)
-        table_subjects(docx, Subject.subjects_to_array(self.subjects))
+        sapproved = []
+        snotapproved = []
+        for subject in self.subjects:
+            if subject.approved:
+                sapproved.append(subject)
+            else:
+                snotapproved.append(subject)
+        if len(sapproved) > 0:
+            paragraph = docx.add_paragraph()
+            paragraph.add_run(self.str_comittee_header)
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+            paragraph.paragraph_format.space_after = Pt(0)
+            self.pcm_answer_approved(paragraph)
+            table_subjects(docx, Subject.subjects_to_array(sapproved))
+        if len(snotapproved) > 0:
+            paragraph = docx.add_paragraph()
+            paragraph.add_run(self.str_comittee_header)
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+            paragraph.paragraph_format.space_after = Pt(0)
+            self.pcm_answer_not_approved(paragraph)
+            table_subjects(docx, Subject.subjects_to_array(snotapproved))
 
     def pcm_analysis(self, docx):
         # pylint: disable=no-member
@@ -84,3 +141,25 @@ class IASI(Request):
             self.academic_program,
             self.academic_period,
             '' if self.is_affirmative_response_advisor_response() else 'no '))
+
+    def pcm_answer_approved(self, paragraph):
+        paragraph.add_run(
+            # pylint: disable=no-member
+            ' APROBAR ').font.bold = True
+        paragraph.add_run(self.str_cm[0].format(
+            # pylint: disable=no-member
+            self.get_academic_program_display(),
+            self.academic_program,
+            self.academic_period,
+            ''))
+
+    def pcm_answer_not_approved(self, paragraph):
+        paragraph.add_run(
+            # pylint: disable=no-member
+            ' NO APROBAR ').font.bold = True
+        paragraph.add_run(self.str_cm[0].format(
+            # pylint: disable=no-member
+            self.get_academic_program_display(),
+            self.academic_program,
+            self.academic_period,
+            'no '))
