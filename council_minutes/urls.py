a7563830d4f310c0e675a129166d691d588c2837
Fortich <ajsuarezf@unal.edu.co>
2020-02-26 14:50:03 -0500

    unused route removed

--- council_minutes/urls.py
+++ council_minutes/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path('allow_generate', views.allow_generate, name='allow_generate'),
     path('generate', views.get_docx_genquerie,
          name='Docx generation by case query'),
-    path('generate_spec', views.generate_spec, name='generate_spec'),
     path('autofill', views.autofill, name='autofill')
 
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
