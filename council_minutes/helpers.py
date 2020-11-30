9b8233c54a0ae611ae9381087383dde731460d5c
Angel Corredor <adcorredorm@unal.edu.co>
2020-11-29 23:36:49 -0500

    Reports first approach

--- council_minutes/helpers.py
+++ council_minutes/helpers.py
@@ -137,3 +137,13 @@ def get_period_choices():
             values.append((st, st))
         choices.extend(values)
     return tuple(reversed(choices))
+
+def querydict_to_dict(query_dict):
+    data = {}
+    for key in query_dict.keys():
+        v = query_dict.getlist(key)
+        if len(v) == 1:
+            v = v[0]
+        data[key] = v
+    return data
+    
